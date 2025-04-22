fbf271d3e081ac57e3e13f17ed500ba0446059e1
Tinkermannn <wilmansaragih2@gmail.com>
2025-04-22 21:03:32 +0700

    Change height on Homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,7 +7,7 @@ export default function Home() {
     return (
         <>
             <div
-                className="w-full h-[450px] bg-cover bg-center bg-no-repeat m-auto"
+                className="w-full h-[550px] bg-cover bg-center bg-no-repeat m-auto"
                 style={{
                     backgroundImage: `url(${Landing})`,
                 }}
