8a3ce8bb21458c4f561a3c5e2ab5c44357e25853
Tinkermannn <wilmansaragih2@gmail.com>
2025-04-29 00:42:31 +0700

    Padding counter

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -19,9 +19,9 @@ export default function Counter() {
 
 
     return (
-        <div className="w-full h-[200px] flex bg-gradient-to-b from-fuchsia-900 via-purple-900 to-fuchsia-900">
+        <div className="w-full h-[300px] flex bg-gradient-to-b from-fuchsia-900 via-purple-900 to-fuchsia-900 py-10">
             <div className='w-full h-full px-10 lg:px-20 flex items-center m-auto max-w-screen-2xl flex-row gap-10'>
-                <div className='w-full h-full flex flex-col items-center gap-5'>
+                <div className='w-full h-full flex flex-col justify-center items-center gap-5'>
                     <div className='text-xl lg:text-3xl font-bold text-white'>Counter Demo</div>
                     <a className='text-white text-bold text-md lg:text-2xl '>{count}</a>
                     <div className='flex flex-row h-full w-42 lg:w-[20%] items-center justify-center text-center gap-5'>
@@ -29,7 +29,7 @@ export default function Counter() {
                             onClick={setDecrement}>-</a>
                         <a className='flex-1 h-12 w-32 bg-white rounded-lg text-fuchsia-700 font-bold hover:bg-fuchsia-200 active:bg-fuchsia-600 active:text-white flex justify-center items-center select-none  cursor-pointer'
                             onClick={() => setCount(0)}>Reset</a>
-                        <a className='flex-none h-10 lg:h-8 w-14 bg-white rounded-lg text-fuchsia-700 font-bold hover:bg-fuchsia-200 active:bg-fuchsia-600 active:text-white select-none  cursor-pointe flex items-center justify-center'
+                        <a className='flex-none h-10 lg:h-8 w-14 bg-white rounded-lg text-fuchsia-700 font-bold hover:bg-fuchsia-200 active:bg-fuchsia-600 active:text-white select-none  cursor-pointer flex items-center justify-center'
                             onClick={setIncrement}>+</a>
                     </div>
                 </div>
